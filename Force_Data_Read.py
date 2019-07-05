5ac08409d39b4e44f5e950f5b9485007cdca598e
yuihjk7412 <514522970@qq.com>
2019-07-05 15:28:48 +0800

    modify baud rate

--- Force_Data_Read.py
+++ Force_Data_Read.py
@@ -7,11 +7,11 @@ import binascii
 if __name__ == '__main__':
     port_Num = input('PLEASE INPUT THE PORT NUMBER(/dev/ttyUSB*):')
     request_Command = bytes.fromhex('01 03 01 C2 00 08 E4 0C')
-    with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 9600, timeout=0.2) as ser:
+    with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.2) as ser:
         print("Serial Port OK!")
     ser.close()
     while 1:
-        with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 9600, timeout=0.2) as ser:
+        with serial.Serial("/dev/ttyUSB%d" % int(port_Num), 115200, timeout=0.2) as ser:
             ser.write(request_Command)
             buf = ser.read(21)
 
